f545f3225788cbd084d9be94fb6e7f360f321f44
Ruben Undheim <ruben.undheim@gmail.com>
2015-03-01 14:21:33 +0100

    Added more man pages

--- debian/man/genmanpages.sh
+++ debian/man/genmanpages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 
-txt2man -t UBERTOOTH-RX      -s 1 ubertooth-rx.txt         > ubertooth-rx.1
-txt2man -t UBERTOOTH-BTLE    -s 1 ubertooth-btle.txt       > ubertooth-btle.1
+txt2man -t UBERTOOTH-RX           -s 1 ubertooth-rx.txt         > ubertooth-rx.1
+txt2man -t UBERTOOTH-BTLE         -s 1 ubertooth-btle.txt       > ubertooth-btle.1
+txt2man -t UBERTOOTH-DEBUG        -s 1 ubertooth-debug.txt      > ubertooth-debug.1
+txt2man -t UBERTOOTH-SPECAN-UI    -s 1 ubertooth-specan-ui.txt  > ubertooth-specan-ui.1
+txt2man -t UBERTOOTH-DFU          -s 1 ubertooth-dfu.txt        > ubertooth-dfu.1
