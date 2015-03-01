1d60be6cb9f2188572980c86756114ce4b50e59f
Ruben Undheim <ruben.undheim@gmail.com>
2015-03-01 14:31:29 +0100

    Added txt file versions of all man pages

--- debian/man/genmanpages.sh
+++ debian/man/genmanpages.sh
@@ -6,3 +6,7 @@ txt2man -t UBERTOOTH-BTLE         -s 1 ubertooth-btle.txt       > ubertooth-btle
 txt2man -t UBERTOOTH-DEBUG        -s 1 ubertooth-debug.txt      > ubertooth-debug.1
 txt2man -t UBERTOOTH-SPECAN-UI    -s 1 ubertooth-specan-ui.txt  > ubertooth-specan-ui.1
 txt2man -t UBERTOOTH-DFU          -s 1 ubertooth-dfu.txt        > ubertooth-dfu.1
+txt2man -t UBERTOOTH-SCAN         -s 1 ubertooth-scan.txt       > ubertooth-scan.1
+txt2man -t UBERTOOTH-FOLLOW       -s 1 ubertooth-follow.txt     > ubertooth-follow.1
+txt2man -t UBERTOOTH-DUMP         -s 1 ubertooth-dump.txt       > ubertooth-dump.1
+txt2man -t UBERTOOTH-UTIL         -s 1 ubertooth-util.txt       > ubertooth-util.1
